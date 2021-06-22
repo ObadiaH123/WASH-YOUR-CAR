3c64df71d3f251008d44114e5eb05ebe1cf7544f
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-06-22 21:49:21 +0300

    modified codes in the js file

--- contact.js
+++ contact.js
@@ -1,4 +1,29 @@
-function validateForm() {
-    var name = document.forms["myForm"]["name"];
-    var p 
+function validate() {
+
+    if( document.myForm.name.value =="" ) {
+        alert( "Please include your name." );
+        document.myForm.name.focus() ;
+        return false;
+    }
+
+    if( document.myForm.pnum.value == "" ) {
+        alert( "Please provide your number." );
+        document.myForm.pnum.focus() ;
+        return false ;
+    }
+
+}
+
+function validateEmail() {
+
+    var email document.myForm.email.value;
+    atpos = email.indexOf("@");
+    dotpos = email.lastIndexOf(".");
+
+    if (atpos < 1 || (dotpos - atpos < 2)) {
+        alert("Please enter correct e-mail.")
+        document.myForm.email.focus();
+        return false;
+    }
+    return true;
 }
