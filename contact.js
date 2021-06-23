6f77bd55660d7a758420598381f34b8b49164a36
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-06-23 09:33:26 +0300

    added the functions necessary for validating the html form

--- contact.js
+++ contact.js
@@ -1,29 +1,70 @@
-function validate() {
+function formValidation() {
 
-    if( document.myForm.name.value =="" ) {
-        alert( "Please include your name." );
-        document.myForm.name.focus() ;
-        return false;
-    }
+    var name = document.myForm.name;
+    var pnum = document.myForm.pnum;
+    var email = document.myForm.email;
+    var male = document.myForm.male;
+    var female = document.myForm.female;
 
-    if( document.myForm.pnum.value == "" ) {
-        alert( "Please provide your number." );
-        document.myForm.pnum.focus() ;
-        return false ;
+    if(allLetter(name)) {
+       
+        if(allNumeric(pnum)) {
+
+            if(validateEmail(email)) {
+
+                if(validateGender(male,female)) {
+
+                }
+            }
+        }
     }
+    return false;
 
 }
 
-function validateEmail() {
-
-    var email document.myForm.email.value;
-    atpos = email.indexOf("@");
-    dotpos = email.lastIndexOf(".");
-
-    if (atpos < 1 || (dotpos - atpos < 2)) {
-        alert("Please enter correct e-mail.")
-        document.myForm.email.focus();
+function allLetter(name) {
+    var letters = /^[A-Za-z]+$/;
+    if(name.value.match(letters)) {
+        return true;
+    }
+    else {
+        alert('Name must have alphabet characters only.');
+        name.focus();
         return false;
     }
-    return true;
+}
+
+function allNumeric(pnum) {
+    var numbers = /^[0-9]+$/;
+    if(pnum.value.match(numbers)) {
+        return true;
+    }
+    else {
+        alert('Phone number is required.');
+        pnum.focus();
+        return false;
+    }
+}
+
+function validateGender(male,female) {
+    x = 0;
+
+    if(male.checked) {
+        x++;
+    }
+    if(female.checked) {
+        x++;
+    }
+
+    if(x == 0) {
+        alert('Select Male/Female');
+        male.focus();
+        return false;
+    }
+
+    else {
+        alert('Form Submitted Succesfully.');
+        window.location.reload()
+        return true;
+    }
 }
