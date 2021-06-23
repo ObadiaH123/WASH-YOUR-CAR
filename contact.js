6f551f7abfa53686e376af283b80572641848cba
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-06-23 12:38:28 +0300

    included responsive alert message for email

--- contact.js
+++ contact.js
@@ -1,2 +1,36 @@
-let form = form.getElementById('myForm');
+const submitBtn = document.getElementById('submit-btn');
+
+const validate = (e) => {
+    e.preventDefault();
+
+    const uname = document.getElementById('uName');
+    const pnum = document.getElementById('pNum');
+    const email = document.getElementById('eMail');
+    const gender = document.getElementById('sex');
+
+    if (uname.value === "");
+     {
+         alert("Please enter your name.");
+         uname.focus();
+         return false;
+     }
+
+     if (pnum.value === "") {
+         alert("Please enter your phone number.");
+         pnum.focus();
+         return false;
+     }
+
+     if (email.value === "") {
+         alert("Please enter your e-mail address.");
+         email.focus;
+         return false;
+     }
+
+     return true;
+
+
+}
+
+submitBtn.addEventListener('click', validate);
 
